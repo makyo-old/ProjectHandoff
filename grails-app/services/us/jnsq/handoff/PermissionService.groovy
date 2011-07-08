bc2088028ea4d5cef245e9dd5cb351edf59e3d7f
unknown <Matthew@.(none)>
2011-07-08 07:10:36 -0600

    Logic

--- grails-app/services/us/jnsq/handoff/PermissionService.groovy
+++ grails-app/services/us/jnsq/handoff/PermissionService.groovy
@@ -6,28 +6,47 @@ class PermissionService {
 
     /*
      * Checks the given permission on the given file for the given actors (if provided)
+     * 
+     * hasPermission(to: t, from: f, intermediary: i, "interact", f)
      */
     def hasPermission(Map actors = [to: null, from: null, intermediary: null], String permission, File file = null) {
-        // Is this cool, a hack, or both?
-        def mask = [read: true, write: true, interact: true]
-        def inverseMask = [read: false, write: false, interact: false]
+        def fp
         
-        // to = mask && ((to.permissions && to.role.permissions) || to.permissions || false)
-        def to = (file == null ? mask : (actors.to == null ? mask : (file.permissions.find { it.actor.id == to.id } ?: inverseMask)))
-        to = (actors.to == null 
-            ? [read: to.read && mask.read, write: to.write && mask.write, interact: to.interact && mask.interact] 
-            : [read: to.read && to.role.permissionsMask.read, write: to.write && to.role.permissionsMask.write, interact: to.interact && to.role.permissionsMask.interact])
+        // to = mask && to.permissions && to.role.permissions
+        fp = FilePermissions.findByFileAndActor(file, actor.to) ?: [read: false, write: false, interact: false]
+        def to = [
+            read: true && (actors.to == null ? true : (actors.to.role.permissionsMask.read && fp.read)),
+            write: true && (actors.to == null ? true : (actors.to.role.permissionsMask.write && fp.write)),
+            interact: true && (actors.to == null ? true : (actors.to.role.permissionsMask.interact && fp.interact))
+        ]
         
-        def from = (file == null ? mask : (actors.from == null ? mask : (file.permissions.find { it.actor.id == from.id } ?: inverseMask)))
-        from = (actors.from == null 
-            ? [read: from.read && mask.read, write: from.write && mask.write, interact: from.interact && mask.interact] 
-            : [read: from.read && from.role.permissionsMask.read, write: from.write && from.role.permissionsMask.write, interact: from.interact && from.role.permissionsMask.interact])
+        fp = FilePermissions.findByFileAndActor(file, actor.from) ?: [read: false, write: false, interact: false]
+        def from = [
+            read: true && (actors.from == null ? true : (actors.from.role.permissionsMask.read && fp.read)),
+            write: true && (actors.from == null ? true : (actors.from.role.permissionsMask.write && fp.write)),
+            interact: true && (actors.from == null ? true : (actors.from.role.permissionsMask.interact && fp.interact))
+        ]
         
-        def interact = (file == null ? mask : (actors.interact == null ? mask : (file.permissions.find { it.actor.id == interact.id } ?: inverseMask)))
-        interact = (actors.interact == null 
-            ? [read: interact.read && mask.read, write: interact.write && mask.write, interact: interact.interact && mask.interact] 
-            : [read: interact.read && interact.role.permissionsMask.read, write: interact.write && interact.role.permissionsMask.write, interact: interact.interact && interact.role.permissionsMask.interact])
+        fp = FilePermissions.findByFileAndActor(file, actor.intermediary) ?: [read: false, write: false, interact: false]
+        def intermediary = [
+            read: true && (actors.intermediary == null ? true : (actors.intermediary.role.permissionsMask.read && fp.read)),
+            write: true && (actors.intermediary == null ? true : (actors.intermediary.role.permissionsMask.write && fp.write)),
+            interact: true && (actors.intermediary == null ? true : (actors.intermediary.role.permissionsMask.interact && fp.interact))
+        ]
         
-        to."$permission" && from."$permission" && intermediary."$permission"
+        to[permission] && from[permission] && intermediary[position]
+    }
+    
+    /*
+     * Sets permissions on a given file for a given actor
+     * 
+     * setPermissions(read: true, write: false, interact: true, actor: a, file: f)
+     */
+    def setPermissions(Map permissionsActorFile) {
+        def permissions = file.permissions.findByActor(actor)
+        permissions = (permissions ?: new FilePermissions())
+        permissions.properties = permissionsActorFile
+        permissions.save(flush: true)
+        permissions
     }
 }
